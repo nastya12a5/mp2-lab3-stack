fad133b5c7a36fd39d0a626e147e379ade122a81
nastya12a5 <mihaleva1208@gmail.com>
2023-11-19 12:08:51 +0300

    add vs

---
+++ mp2-lab3-stack/mp2-lab3-stack.cpp
@@ -0,0 +1,14 @@
+﻿// mp2-lab3-stack.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "TStack.h"
+/* Написана реалтзация класса TStack 
+*/
+int main()
+{
+    TStack <int> st;
+
+}
+
+
